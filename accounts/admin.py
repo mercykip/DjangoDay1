0124e08b75c30c946171fadac291cf1606a806fa
Mercy Kipyegon <mercyjemosop@gmail.com>
2020-08-19 22:53:49 +0300

    added django documentation

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+#relative import
+from .models import Account
+admin.site.register(Account)
--- accounts/models.py
+++ accounts/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Account(models.Model):
-    title           =   models.TextField()
-    description     =   models.TextField()
+    title           =   models.CharField(max_length=120)#max_length=required
+    description     =   models.TextField(blank=True,null=True)
+    amount          =   models.FloatField()
+    summary         =   models.TextField(default='this is cool')
+    maritalstatatus =   models.BooleanField(null=True)
